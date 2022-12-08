d0b74ce5f0ad3529f6f7b116ad20b1ad53476aff
kor-bim <dbsgksqlschl@gmail.com>
2022-12-08 17:54:51 +0900

    Feat: 데이터베이스 커넥션 추가

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -9,7 +9,39 @@ export const databaseConfig: TypeOrmModuleAsyncOptions = {
     port: configService.get<number>('MYSQL_DB_PORT'),
     username: configService.get<string>('MYSQL_DB_USER'),
     password: configService.get<string>('MYSQL_DB_PASSWORD'),
-    database: configService.get<string>('MYSQL_DB_SCHEME'),
+    database: configService.get<string>('MYSQL_DB_SCHMEA_1'),
+    synchronize: configService.get<boolean>('MYSQL_DB_SYNCHRONIZE'),
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    logging: true
+  }),
+  inject: [ConfigService]
+}
+
+export const database2Config: TypeOrmModuleAsyncOptions = {
+  imports: [ConfigModule],
+  useFactory: (configService: ConfigService) => ({
+    type: 'mysql',
+    host: configService.get<string>('MYSQL_DB_HOST'),
+    port: configService.get<number>('MYSQL_DB_PORT'),
+    username: configService.get<string>('MYSQL_DB_USER'),
+    password: configService.get<string>('MYSQL_DB_PASSWORD'),
+    database: configService.get<string>('MYSQL_DB_SCHMEA_2'),
+    synchronize: configService.get<boolean>('MYSQL_DB_SYNCHRONIZE'),
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    logging: true
+  }),
+  inject: [ConfigService]
+}
+
+export const database3Config: TypeOrmModuleAsyncOptions = {
+  imports: [ConfigModule],
+  useFactory: (configService: ConfigService) => ({
+    type: 'mysql',
+    host: configService.get<string>('MYSQL_DB_HOST'),
+    port: configService.get<number>('MYSQL_DB_PORT'),
+    username: configService.get<string>('MYSQL_DB_USER'),
+    password: configService.get<string>('MYSQL_DB_PASSWORD'),
+    database: configService.get<string>('MYSQL_DB_SCHMEA_3'),
     synchronize: configService.get<boolean>('MYSQL_DB_SYNCHRONIZE'),
     entities: [__dirname + '/**/*.entity{.ts,.js}'],
     logging: true
